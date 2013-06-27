e12c9dd20a8b4019d7a2cea5607407791cdcd68c
Néstor Coppi <nestorcoppi@gmail.com>
2013-06-27 02:40:34 -0300

    Arreglado Datepicker + autocomplete

--- app/inputs/date_picker_input.rb
+++ app/inputs/date_picker_input.rb
@@ -1,9 +1,5 @@
 class DatePickerInput < SimpleForm::Inputs::Base
   def input
-    if object.respond_to?(attribute_name) && object.send(attribute_name)
-      value = I18n.l(object.send(attribute_name))
-    end
-    
     @builder.text_field(
       attribute_name, input_html_options.reverse_merge(default_attributes)
     ).html_safe
@@ -12,8 +8,12 @@ class DatePickerInput < SimpleForm::Inputs::Base
   private
 
   def default_attributes
+    value = I18n.l(
+      object.send(attribute_name)
+    ) if object && object.send(attribute_name)
+
     {
-      value: (I18n.l(object.send(attribute_name)) if object.send(attribute_name)),
+      value: value,
       autocomplete: 'off',
       data: { date_picker: true }
     }
