4f1aca2604cffa652e481f7720315a509c3ed063
Néstor Coppi <nestorcoppi@gmail.com>
2013-06-27 00:45:03 -0300

    Error al mergiar

--- app/inputs/date_picker_input.rb
+++ app/inputs/date_picker_input.rb
@@ -5,16 +5,7 @@ class DatePickerInput < SimpleForm::Inputs::Base
     end
     
     @builder.text_field(
-<<<<<<< HEAD
-      attribute_name,
-      input_html_options.reverse_merge(
-        value: value,
-        autocomplete: 'off',
-        data: { 'date-picker' => true }
-      )
-=======
       attribute_name, input_html_options.reverse_merge(default_attributes)
->>>>>>> adc69e9859c788ca16d5fe1ec43a2896703456c5
     ).html_safe
   end
 
