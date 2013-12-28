0e26f405057ccc60cce80e2ab2e2404eb3446403
Néstor Coppi <nestorcoppi@gmail.com>
2013-12-28 16:19:42 -0300

    Capifying... (3.0)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,33 @@
-require 'bundler/capistrano'
-
 set :application, 'abaco'
-set :repository,  'https://github.com/cetem/abaco.git'
-set :deploy_to, '/var/rails/abaco'
 set :user, 'deployer'
-set :group_writable, false
-set :shared_children, %w(log)
-set :use_sudo, false
+set :repo_url, 'https://github.com/cetem/abaco.git'
 
 set :scm, :git
-set :branch, 'master'
+set :deploy_to, '/var/rails/abaco'
+set :deploy_via, :remote_cache
 
-role :web, 'finanzas-cetem.frm.utn.edu.ar'
-role :app, 'finanzas-cetem.frm.utn.edu.ar'
-role :db, 'finanzas-cetem.frm.utn.edu.ar', primary: true
+set :format, :pretty
+set :log_level, :info
 
-before 'deploy:finalize_update', 'deploy:create_shared_symlinks'
+set :linked_files, %w{config/app_config.yml}
+set :linked_dirs, %w{log private}
+
+set :keep_releases, 5
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  
-  task :restart, roles: :app, except: { no_release: true } do
-    run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
   end
 
-  desc 'Creates the symlinks for the shared folders'
-  task :create_shared_symlinks, roles: :app, except: { no_release: true } do
-    shared_paths = [['private'], ['config', 'app_config.yml']]
-
-    shared_paths.each do |path|
-      shared_files_path = File.join(shared_path, *path)
-      release_files_path = File.join(release_path, *path)
-
-      run "ln -s #{shared_files_path} #{release_files_path}"
+  desc 'Update crontab with whenever'
+  after :finishing, 'deploy:cleanup' do
+    on roles(:all) do
+      within release_path do
+        execute :rake, 'tmp:clear'
+      end
     end
   end
 end
