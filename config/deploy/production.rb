87124387d1ead4a7af4fa8d02d2fcb66d1464b1f
Néstor Coppi <nestorcoppi@gmail.com>
2014-02-22 11:26:10 -0300

    Mejora en capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,8 @@
 set :stage, :production
+set :rails_env, 'production'
 
-role :all, %w{finanzas-cetem.frm.utn.edu.ar}
+role :web, %w{finanzas-cetem.frm.utn.edu.ar}
+role :app, %w{finanzas-cetem.frm.utn.edu.ar}
+role :db, %w{finanzas-cetem.frm.utn.edu.ar}
 
 server 'finanzas-cetem.frm.utn.edu.ar', user: 'deployer', roles: %w{web app db}
